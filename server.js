e4a04c98b40dde9d2992fb1cfdf5d21993d90bbd
Addy Abdelghani <mmt.shady@gmail.com>
2021-08-09 19:14:07 +0100

    initial db

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
+const connectDB = require('./config/db');
 
 const app = express();
 // app.use(express.json());
+connectDB();
 
 // Routes
 const productsRoute = require('./routes/products');
